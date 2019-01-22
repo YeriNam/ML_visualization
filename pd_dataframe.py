f53d1756566f5602fc3ed259697a1cd932951981
YeriNam <45750930+YeriNam@users.noreply.github.com>
2019-01-23 08:50:34 +0900

    Update pd_dataframe.py

--- pd_dataframe.py
+++ pd_dataframe.py
@@ -8,6 +8,17 @@ df = pd.merge(left=ratings, right=parking, on="placeID", how="left")
 
 #complex indexing 
 midx = pd.MultiIndex(levels=[['lama', 'cow', 'falcon'],
-['speed', 'weight', 'length']],
-...                      labels=[[0, 0, 0, 1, 1, 1, 2, 2, 2],
-...                              [0, 1, 2, 0, 1, 2, 0, 1, 2]])
+                             ['speed', 'weight', 'length']],
+                     labels=[[0, 0, 0, 1, 1, 1, 2, 2, 2],
+                             [0, 1, 2, 0, 1, 2, 0, 1, 2]])
+df = pd.DataFrame(index=midx, columns=['big', 'small'],
+                  data=[[45, 30], [200, 100], [1.5, 1], [30, 20],
+                        [250, 150], [1.5, 0.8], [320, 250],
+                        [1, 0.8], [0.3,0.2]])
+
+#groupby 
+# Group by the parking_lot column
+parking_group = df.groupby("parking_lot")
+
+# Calculate the mean ratings
+parking_group["service_rating"].mean()
