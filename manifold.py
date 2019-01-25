d57b52b50406525274e2ecee58bdbb4a38d56c3c
YeriNam <45750930+YeriNam@users.noreply.github.com>
2019-01-25 16:06:22 +0900

    Update manifold.py

--- manifold.py
+++ manifold.py
@@ -9,8 +9,8 @@ mani_trainx=trainx[['MaturitySize','FurLength','Breed1','Vaccinated']]
 Y = manifold.TSNE(n_components=n_components,init='pca',perplexity=50).fit_transform(mani_trainx)
 
 fig = plt.figure(figsize=(15, 8))
-plt.suptitle("Manifold Learning with %i points, %i neighbors"
-             % (1000, n_neighbors), fontsize=14)
+#plt.suptitle("Manifold Learning with %i points, %i neighbors"
+#             % (1000, n_neighbors), fontsize=14)
 
 print("tsne")
 ax = fig.add_subplot(257)
@@ -20,7 +20,7 @@ scale_z=1
 #ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), np.diag([scale_x, scale_y, scale_z, 1]))
 ax.scatter(Y[:, 0], Y[:,1], c=target, cmap=plt.cm.Spectral)
 #ax.view_init(4, -72)
-plt.title("Isomap")
+plt.title("t-SNE")
 #ax.xaxis.set_major_formatter(NullFormatter())
 #ax.yaxis.set_major_formatter(NullFormatter())
 #plt.axis('tight')
